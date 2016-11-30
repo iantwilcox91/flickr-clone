e9ef8f2a6d717d1f2ba0072b8c529a9116d87537
Ian <bunkadoo91@gmail.com>
2016-11-30 15:17:31 -0800

    can add pictures

--- src/FlickrClone/Controllers/UserController.cs
+++ src/FlickrClone/Controllers/UserController.cs
@@ -3,17 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using FlickrClone.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace FlickrClone.Controllers
 {
+    //the [Authorize] attribute on UserController allows access to the controller only if a user is logged in
+    [Authorize]
     public class UserController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public async Task<IActionResult> Index()
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            return View(_db.Pictures.Where(x => x.User.Id == currentUser.Id));
+        }
+        public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Picture picture)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            picture.User = currentUser;
+            _db.Pictures.Add(picture);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
